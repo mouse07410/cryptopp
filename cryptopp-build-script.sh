0f0d201d1cc8b60cc2c03db2905eb8efbdec7952
Mouse <mouse008@gmail.com>
2017-05-12 00:50:27 -0400

    Renamed building script

---
+++ cryptopp-build-script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cryptopp-remove && make clean && make -j 4 all && make test && sudo make install PREFIX=/opt/local && make clean && CXX=g++ make -j 4 all && make test && sudo cp libcryptopp.a /opt/local/lib/libcryptopp-gcc.a
