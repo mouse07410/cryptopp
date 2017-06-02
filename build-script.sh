9bc025df99c65f30ff32908bf149e012914be3df
Mouse <mouse008@gmail.com>
2017-06-01 22:27:33 -0400

    Changed "make clean" to "make distclean"

--- build-script.sh
+++ build-script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cryptopp-remove 
-make clean && make -j 4 all && make test && sudo make install PREFIX=/opt/local && make clean && CXX=g++ make -j 4 all && make test && sudo cp libcryptopp.a /opt/local/lib/libcryptopp-gcc.a
+make distclean && make -j 4 all && make test && sudo make install PREFIX=/opt/local && make distclean && CXX=g++ make -j 4 all && make test && sudo cp libcryptopp.a /opt/local/lib/libcryptopp-gcc.a
