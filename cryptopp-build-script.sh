fd2f92bce1b1b13334c5f60af4c6685140225d9b
Mouse <mouse008@gmail.com>
2018-12-01 18:44:32 -0500

    Added creating build log to the auto-build script

--- cryptopp-build-script.sh
+++ cryptopp-build-script.sh
@@ -2,4 +2,4 @@
 
 cryptopp-remove 
 sudo rm -f /opt/local/lib/libcryptopp.a /opt/local/lib/libcryptopp.dylib || true
-make distclean && make -j4 all test && sudo -EH make install PREFIX=/opt/local && sudo mv /opt/local/lib/libcryptopp.a /opt/local/lib/libcryptopp-clang.a && sudo rm -f /opt/local/lib/libcryptopp.dylib && make distclean && CXX=g++ make -j4 all test && sudo cp libcryptopp.a /opt/local/lib/libcryptopp-gcc.a && sudo ln -s /opt/local/lib/libcryptopp-clang.a /opt/local/lib/libcryptopp.a
+make distclean && make -j4 all test 2>&1 | tee make-clang-out.txt && sudo -EH make install PREFIX=/opt/local && sudo mv /opt/local/lib/libcryptopp.a /opt/local/lib/libcryptopp-clang.a && sudo rm -f /opt/local/lib/libcryptopp.dylib && make distclean && CXX=g++ make -j4 all test 2>&1 | tee make-gcc-out.txt && sudo cp libcryptopp.a /opt/local/lib/libcryptopp-gcc.a && sudo ln -s /opt/local/lib/libcryptopp-clang.a /opt/local/lib/libcryptopp.a
