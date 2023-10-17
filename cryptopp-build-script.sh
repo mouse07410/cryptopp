f966550050d7af2138563714e8c4a3313afd2cd0
Mouse <mouse008@gmail.com>
2023-10-17 18:24:32 -0400

    Remove installed .dylib

--- cryptopp-build-script.sh
+++ cryptopp-build-script.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
 cryptopp-remove 
+
+# Build Clang version
 sudo rm -f /opt/local/lib/libcryptopp.a /opt/local/lib/libcryptopp.dylib || true
-make distclean && make -j4 all test 2>&1 | tee make-clang-out.txt && sudo -EH make install PREFIX=/opt/local && sudo mv /opt/local/lib/libcryptopp.a /opt/local/lib/libcryptopp-clang.a && sudo rm -f /opt/local/lib/libcryptopp.dylib && make distclean && CXX=g++ make -j4 all test 2>&1 | tee make-gcc-out.txt && sudo cp libcryptopp.a /opt/local/lib/libcryptopp-gcc.a && sudo ln -s /opt/local/lib/libcryptopp-clang.a /opt/local/lib/libcryptopp.a
+make distclean && make -j4 all test 2>&1 | tee make-clang-out.txt && sudo -EH make install PREFIX=/opt/local && sudo mv /opt/local/lib/libcryptopp.a /opt/local/lib/libcryptopp-clang.a 
+
+
+# Delete .dylib to avoid potential interference between libraries built by Clang++ and G++
+sudo rm -f /opt/local/lib/libcryptopp.dylib && 
+
+# Build GCC version
+#make distclean && CXX=g++ make -j4 all test 2>&1 | tee make-gcc-out.txt && sudo cp libcryptopp.a /opt/local/lib/libcryptopp-gcc.a 
+
+# Make Clang version system default
+sudo ln -s /opt/local/lib/libcryptopp-clang.a /opt/local/lib/libcryptopp.a
